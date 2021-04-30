8b46b1643fc37e7c549f9a9b3cf8e4cd0c21b8ba
CJ <80271834+CodeR32-dev@users.noreply.github.com>
2021-04-29 21:52:40 -0600

    Create Starship-Qwerty_OPS.pl
    optional data please see me for more information or simply like this github repo or whatever you got it all in the end remember that. OSS EXPRESS ART

---
+++ AvesCB/FTL/Starship-Qwerty_OPS.pl
@@ -0,0 +1,37 @@
+use readonly;                                #TODO# exit() !exit->goto open exit              >:(y):<                 
+#*#                                          #TODO# !Thanks                                      ^
+use strict; (Compiler Standards) #&#         #TODO# -> ___a____                                   \
+#^# Warnings :*use*: Functional Perl         #TODO#   ->    ___b____                               \
+use warnings; #*# Show Warning (MORE)        #TODO#      ->          ___c____                      /
+#*#                                          #TODO#         ->              ___d____               \
+#*# requirements and other goodies           #TODO#            ->                  ___e____         \|/
+!== require syscall caller stat              #TODO#            ->           ___f____                /
+!== require import open => service           #TODO#         ->      ___g____                       /
+BEGIN: #TODO# Read This Script {...}         #TODO#    ->   ___h____                              /
+#*#                                          #TODO# ->  ___i____                                  \
+for open next goto $Available->*Window* @ROI                                                       \
+for my @ROI !== return !==:*ON*: %Investment                                                        \
+#*#                                                                                                  \|/
+!== import @Src_code_inet #&# >      inet.msg* #&# > 1 {...} #*# CB-11 :switch: R32 <- connect         \
+!== import @Src_code_inet #&# >  inet.msg.ext* #&# > 2 {...} #*# CB-22 :switch: R31 >  @ID             /
+!== import @Src_code_inet #&# >  inet.msg.com* #&# > 3 {...} #*# CB-33 :switch: R31 >  @ID            /
+!== import @Src_code_inet #&# > inet.msg.rand* #&# > 4 {...} #*# CB-44 :switch: R32 <- index          \
+#*#                                                                                                    \
+@Require->%Special_Route #TODO# @SOBJ !== require %Special_Route set=default:route+special*           /
+#*#                                                                                                 /
+%Special_Route => Route_Special() via->connect.index.map \@ref push->@Path                          \
+:select: @Next-> goto open next $Available->*Window* #*# Optional                                    \|/
+for next .push *Window* && *Peek* !==@GUIX for bless MENU.log !seek->open                            /
+#*#                                                                                                  \
+goto ref:map.connect :: [ref]->[map]->[connect]->[mod]->[index] #*# <Example>                        /
+#*#                                                                                                  \   
+sub CTRL_32-A {...} == $path open '~//src/code/temp/OSS' #?# <C> <:> <:> :|: <:> <:> <C> #?#         /
+sub CTRL_32-B {...} == $path open '~//src/code/temp/OSS' #?# <:> <C> <:> :|: <:> <C> <:> #?#         \
+sub CTRL_32-C {...} == $path open '~//src/code/temp/OSS' #?# <:> <:> <C> :|: <C> <:> <:> #?#         / 
+#*#                                                                                                 /
+return 0; 1; #&# Functional Values        #&#  GUIX -> AVES                                         \|/
+END;         #&# Return Script Ending     #&#  CTRL -> LOGY                                          \
+#*#           #~!@#$%^&*()_+QWERTY__UI->OPS:GOGO .FTW .LTR .LTS .OSS .EN .CODE .US .OPTSUS.OpenToken  \
+#*# !Thanks ...                                                                                       /
+#!# {<Sigil>} {\\\} {:|:} {///} 'To Be Continued' {...} {<:|:>} {::} {**}                            /
+#^# !Enjoy {...}                     COMPASS NAVI UNIVERSAL [!CYA:CODY]-> ^&GOGO.LTR.LTS.OSS    <:|:> 
