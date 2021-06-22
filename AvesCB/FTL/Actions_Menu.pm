61cd1ea0324eac3e84150d7392a50d6b01d5724f
Cody B <80271834+CodeR32-dev@users.noreply.github.com>
2021-06-21 18:02:16 -0600

    Create Actions_Menu.pm
    Create Options Menu

---
+++ AvesCB/FTL/Actions_Menu.pm
@@ -0,0 +1,32 @@
+                                       BEGIN:
+import open system;
+        use system;
+      use readonly;
+
+@READ ref => \@mode
+define CB_CODE(b, c, len) {		        				\#
+	INT __i__;							    			\#
+	for (__i__ = 0; __i__ < (INT)(len); __i__++)    	\#
+			(cb2)[__i__] ^= (cb1)[__i__];				\#
+}
+    @Optsus => goto -> LINK - NOTE - FIXME - TODO -
+print "OPTSUS-ID MARS PRESERVE TEAM SUPPORT && SERVICES NETWORK"
+say '$:> :: ... :: :. :|: .: :: ... ::'
+print "Connecting you now ...."
+for :connect: :select: :map: @PORT_NUMBER_LIST_MENU <?>
+close->$Port:7890; eq []  ->    a(.route)+(.path)==(connect.index.map) # Destination
+open<-$Port:7891;  eq []   <-   b(.route)+(.path)==(connect.index.map) # Route
+close->$Port:7892; eq []  ->    c(.route)+(.path)==(connect.index.map) # Path
+open<-$Port:7893;  eq []   <-   d(.route)+(.path)==(connect.index.map) # route
+close->$Port:7894; eq []  ->    e(.route)+(.path)==(connect.index.map) # Eq
+open<-$Port:7895;  eq []   <-   f(.route)+(.path)==(connect.index.map) # Alphabet
+close->$Port:7896; eq []  ->    g(.route)+(.path)==(connect.index.map) # $Port
+open<-$Port:7897;  eq []   <-   h(.route)+(.path)==(connect.index.map) # Options
+close->$Port:7898; eq []  ->    i(.route)+(.path)==(connect.index.map) # Numbers
+open<-$Port:7899;  eq []   <-   j(.route)+(.path)==(connect.index.map) # Arrows
+close->$Port:7890; eq []  ->    k(.route)+(.path)==(connect.index.map) # Binds
+open<-$Port:7891;  eq []   <-   l(.route)+(.path)==(connect.index.map) # Brackets
+ @pass:  @value;
+@value:   @pass;
+return 1;
+                                         END;
