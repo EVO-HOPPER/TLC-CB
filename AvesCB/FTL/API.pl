04f07c14ca48ee9fc3db93830429febdb8327df4
Cody B <80271834+CodeR32-dev@users.noreply.github.com>
2021-06-28 15:23:05 -0600

    Create API.pl
    Create an API GUIX perl module

---
+++ AvesCB/FTL/API.pl
@@ -0,0 +1,120 @@
+use strict;#
+A(eq~~)# Stream AIO Verify Check
+use warnings;#
+B(eq!!)# Statement Verify Check
+use readonly;#
+C(eq @ @)# Id Object Verify Check
+use visuals;#
+D(eq##)# Comment Verify Check
+for\ our - > < TAG > ;#
+E(eq $$)# Variable Verify Check
+for\ our - > {
+    CODE
+};#
+F(eq % % )# Hash Verify Check
+for\ our - > (OBJECT);#
+G(eq ^ ^ )# Directional Verify Check
+for\ our - > [BOX];#
+H(eq && )# Routine Verify Check
+use {
+    Token_Opts
+};#
+I(eq ** )# RandGenStar Verify Check
+fetch() USER.CODY
+for: S: * Y *: fork - > connect.map()
+for syntax - > {
+    eq[fx($Cody) < C > ]
+};
+[use] system => eq {
+    ['~//perl.pm']
+};
+use mapper()
+goto MAPPER[select] * map - > * module !== require * CODE *
+    for my map() connect: index: map => ref.@CODE;
+import @ENV;\
+ref sub console {
+    import open ENV system goto our * perl *
+        use\ my[select] NOTE
+    for NOTE ** *
+        say['Getting Lost With You In Outer Space']
+} && do execl < C >
+    else exit $Program;
+for my $Program !== AVES.pm *
+    NOTE - {
+        "Given Thanks Space Time Ace"
+    };
+sub Console_Sum1 {
+    sub CTRL {
+        ...
+    };#
+    STUB - [($456)][($DEF)][($32)][($BB)]
+    LINK - [1] + [4] + [7] + [10] - > (A)(D)(G) => {
+        ADG
+    } + {
+        1 A
+    } == [{
+        eq: ($SUM1)
+    }]
+    for $SUM1 == {
+        '22ADG1A'
+    }
+}#
+This Link
+sub Console_Sum2 {
+    sub Station {
+        ...
+    };#
+    STUB - [($123)][($ABC)][($32)][($AA)]
+    LINK - [2] + [5] + [8] + [11] - > (B)(E)(H) => {
+        BEH
+    } + {
+        2 B
+    } == [{
+        eq: ($SUM2)
+    }]
+    for $SUM1 == {
+        '26BEH2B'
+    }
+}
+sub Console_Sum3 {
+    sub Console {
+        ...
+    };#
+    STUB - [($789)][($GHI)][($32)][($CC)]
+    LINK - [3] + [6] + [9] + [12] - > (C)(F)(I) => {
+        CFI
+    } + {
+        3 C
+    } == [{
+        eq: ($SUM3)
+    }]
+    for $SUM1 == {
+        '30CFI3C'
+    }
+}
+TODO - Return Functional Values Next
+FIXME - ['Outer.space\.src']
+say {
+    "Please fix any code you dont understand thanks again!..."
+}
+FIXME - {
+    'Change.log\ref/map => *index:connect:map*'
+}
+print {
+    'Hostname for Server OPTS'
+} !==
+require % pass; !==
+require % value;
+bless @pass: % value;
+bless @value: % pass;
+return @Pass;
+return @Value;
+for @Pass and do next
+return $Next_Line;
+for my @Value - > $FILE_AVES_TRANSLATION;\
+do <C > .this - > {
+    ':|: connect [select] index.map'
+}
+return 1;
+END;
+for my sub REVIEW - console station CTRL Readonly[@FILE_AVES_TRANSLATION]
