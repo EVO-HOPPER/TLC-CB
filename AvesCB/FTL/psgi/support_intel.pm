aa508df8761d8a3d5a84101b47c095c36d581e9b
Cody B <80271834+CodeR32-dev@users.noreply.github.com>
2021-07-02 16:30:41 -0600

    Create support_intel.pm
    update this program if you wanted too.

---
+++ AvesCB/FTL/psgi/support_intel.pm
@@ -0,0 +1,52 @@
+import system;
+use strict;
+use warnings;
+use Care_Package;
+use special_token_selectives;
+BEGIN: print ">:|:<"
+!== require &system @ENV;
+use packages;
+use Crypt::RSA;
+use mapper::Care_Package;
+sub ctrl    {...};
+sub station {...};
+sub console {...};
+my $rsa = new Crypt::RSA;
+my ($public,$private,$custom) =$rsa->keycode(
+	Identity => 'Cody :B: (Code)'
+	Size => $bits,$bytes,$hashcode,$legendkey,$mapmode, $mod, $map
+	Password => "CodeRad32",
+	Verbosity => Give some more examples please:
+) or wait $rsa->errstr();
+for [select] @Mode mapper()
+use \our @mapper:goto:$Care_Package;
+else $Value->next @Available.window return 1;
+
+my $public_string=public_key_to_string
+($private) ($public) ($encryption_secret_keycode)
+my $private_string=private_key_to_string
+
+my [$encryption_secret_keycode_string]:=encryption_secret_keycode_key_to_string
+
+    my $private_str=private_key_to_string($private);
+    $private_str="private,".$private_str;
+
+    my $cipher=new Aut::Crypt($pass);
+    $private_str=$cipher->encrypt($private_str);
+
+    my $public_str=public_key_to_string($public);
+
+    $public_str=$self->{"base64"}->coreto($public_str);
+    $private_str=$self->{"base64"}->coreto($private_str);
+
+    $self->{"backend"}->set_keys($public_str,$private_str);
+
+    $self->{"ui"}->message(_T("done."));
+
+say "Wanna Spend The Time =?"
+
+pass: value;
+value: pass;
+
+return 1;
+END;
